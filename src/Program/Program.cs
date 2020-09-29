45a1b9af6d9ac7412cc304db7be046a20ff0a081
Eduardo Daniel Pérez Hunich <eduardo.perez.2151@gmail.com>
2020-09-29 19:44:12 -0300

    Update credentials

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,8 +6,8 @@ namespace WhatsAppApiUCU
     {
         static void Main(string[] args)
         { 
-            const string accountSid = "ACa8395e7223e7719988c09db8dfb68f0f";
-            const string authToken = "fdcaac12dc7e5b031908429ee402e7a4";
+            const string accountSid = "AC52493b284c82ba2c6b196ef82e412576";
+            const string authToken = "fdbb56b4123831013cce62fd5686bc05";
 
             var whatsApp = new WhatsAppApi(accountSid, authToken);
             string sid = whatsApp.Send("+598<Poné tu teléfono acá>", "Hey there! I'm using WhatsAppUCU");
